82e51573269a40c11d0b98aed701d46cbde64e85
Edivaldo Reis <efrj@hotmail.com>
2025-05-18 20:28:44 -0300

    Added players.rb

---
+++ data/players.rb
@@ -0,0 +1,12 @@
+@player_names = [
+  'Ana Silva', 'Bruno Oliveira', 'Carlos Santos', 'Débora Lima',
+  'Eduardo Pereira', 'Fátima Costa', 'Gabriel Martins', 'Helena Cruz',
+  'Igor Almeida', 'Juliana Souza', 'Kleber Ramos', 'Larissa Gomes',
+  'Marcelo Dias', 'Natália Ferreira', 'Otávio Ribeiro', 'Paula Barbosa',
+  'Quentin Tarantino', 'Roberta Carvalho', 'Sérgio Mendes', 'Talita Rocha'
+]
+
+@avatar_icons = [
+  'user', 'user-tie', 'user-graduate', 'user-ninja', 'user-astronaut',
+  'user-secret', 'user-md', 'user-nurse', 'user-shield', 'user-alt'
+]
